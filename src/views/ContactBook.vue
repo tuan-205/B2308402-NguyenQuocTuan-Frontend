1fa0cf7989c5f85d99a2b72ab299f1feb3309879
tuan-205 <10nqtuan05@gmail.com>
2026-06-19 11:11:40 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/views/ContactBook.vue
@@ -0,0 +1,130 @@
+<script>
+import ContactCard from '@/components/ContactCard.vue';
+import ContactList from '@/components/ContactList.vue';
+import InputSearch from '@/components/InputSearch.vue';
+import ContactService from '@/services/contact.service';
+
+export default {
+    components:{
+        ContactCard,
+        InputSearch,
+        ContactList,
+    },
+    data(){
+        return{
+            contacts: [],
+            activeIndex: -1,
+            searchText: "",
+        };
+    },
+    watch: {
+        // Giam sat cac thay doi cua bien searchText
+        // Bo chon phan tu dang duoc chon trong danh sach
+        searchText(){
+            this.activeIndex = -1;
+        },
+    },
+    computed: {
+        // Chuyen cac doi tuong contact thanh chuoi de tien cho tim kiem
+        contactStrings(){
+            return this.contacts.map((contact) => {
+                const {name, email, address, phone} = contact;
+                return [name, email, address, phone].join("");
+            });
+        },
+        // Tra ve cac contact co chua thong tin can tim kiem
+        filteredContacts(){
+            if(!this.searchText) return this.contacts;
+            return this.contacts.filter((_contact, index) => {
+                return this.contactStrings[index].includes(this.searchText)
+            });
+        },
+        activeContact(){
+            if(this.activeIndex < 0) return null;
+            return this.filteredContacts[this.activeIndex];
+        },
+        filteredContactsCount(){
+            return this.filteredContacts.length;
+        },
+    },
+    methods: {
+        async retrieveContacts(){
+            try{
+                this.contacts = await ContactService.getAll();
+            } catch (error){
+                console.log(error);
+            }
+        },
+        refreshList(){
+            this.retrieveContacts();
+            this.activeIndex = -1;
+        },
+        async removeAllContacts(){
+            if(confirm("Bạn có muốn xóa tất cả Liên hệ?")){
+                try{
+                    await ContactService.deleteAll();
+                    this.refreshList();
+                } catch (error){
+                    console.log(error);
+                }
+            }
+        },
+        goToAddContact(){
+            this.$router.push({name: "contact.add"});
+        },
+    },
+    mounted(){
+        this.refreshList();
+    },
+};
+</script>
+
+<style scoped>
+.page{
+    text-align: left;
+    max-width: 750px;
+}
+</style>
+
+<template>
+    <div class="page row">
+        <div class="col-md-10">
+            <InputSearch v-model="searchText" />
+        </div>
+        <div class="mt-3 col-md-6">
+            <h4>
+                Danh bạ
+                <i class="fas fa-address-book"></i>
+            </h4>
+            <ContactList
+            v-if="filteredContactsCount > 0"
+            :contacts="filteredContacts"
+            v-model:activeIndex="activeIndex"
+            />
+            <p v-else>Không có liên hệ nào.</p>
+
+            <div class="mt-3 row justify-content-around align-items-center">
+                <button class="btn btn-sm btn-primary" @click="refreshList()">
+                    <i class="fas fa-redo"></i> Làm mới
+                </button>
+
+                <button class="btn btn-sm btn-success" @click="goToAddContact">
+                    <i class="fas fa-plus"></i> Thêm mới
+                </button>
+
+                <button class="btn btn-sm btn-danger" @click="removeAllContacts">
+                    <i class="fas fa-trash"></i> Xóa tất cả
+                </button>
+            </div>
+        </div>
+        <div class="mt-3 col-md-6">
+            <div v-if="activeContact">
+                <h4>
+                    Chi tiết liên hệ
+                    <i class="fas fa-address-"></i>
+                </h4>
+                <ContactCard :contact="activeContact" />
+            </div>
+        </div>
+    </div>
+</template>
